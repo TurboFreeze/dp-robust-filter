b1b7aa34af8880018035fecac5cdbc37829f6d82
TurboFreeze <iturbofreeze@gmail.com>
2020-06-17 00:27:06 -0400

    proper gaussian mechanism

--- dpCode/dpFilterGaussianMean.m
+++ dpCode/dpFilterGaussianMean.m
@@ -3,7 +3,7 @@ function [ dpEstMean ] = dpFilterGaussianMean(data, gamma, tau, cher, C, eps)
     
     % laplace mechanism
     kappa = gamma / (1 - 2 * gamma) + (sqrt(2) * gamma + sqrt(2 * gamma)) / (1 - 2 * gamma);
-    bound = 2 * sqrt(2 * log(1.25 / tau)) * ((3 + 2 * sqrt(gamma)) * kappa + 2 * gamma * sqrt(C * log(1 / gamma)));
-    noise = laplaceSample(2 * bound / eps, length(estMean));
+    bound = sqrt(2 * log(1.25 / tau)) * ((3 + 2 * sqrt(gamma)) * kappa + 2 * gamma * sqrt(C * log(1 / gamma)));
+    noise = normrnd(0, 2 * bound / eps, length(estMean));
     
     dpEstMean = estMean + noise;
